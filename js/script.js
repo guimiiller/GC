b6d755280ccfd62480638b1fb7599359829cb0a1
guimiiller <guilhermemillerblack@gmail.com>
2024-05-18 12:55:10 -0300

    add script of menu

--- js/script.js
+++ js/script.js
@@ -478,3 +478,26 @@ function animeScroll() {
         }
     });
 }
+
+/* MENU */
+const menu = document.querySelector('.menu')
+const navMenu = document.querySelector('.nav-menu')
+
+menu.addEventListener('click', () =>{
+    menu.classList.toggle('ativo')
+    navMenu.classList.toggle('ativo')
+})
+
+document.addEventListener("DOMContentLoaded", function() {
+    
+    var menuLinks = document.querySelectorAll('.nav-menu a');
+
+    
+    menuLinks.forEach(function(menuLink) {
+        menuLink.addEventListener('click', function() {
+            
+            document.querySelector('.menu').classList.remove('ativo');
+            document.querySelector('.nav-menu').classList.remove('ativo');
+        });
+    });
+});
